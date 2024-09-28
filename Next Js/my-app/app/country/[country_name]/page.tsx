eba8981ae7810ee8cf3bc44ec14e5f90729fe56a
Abdul Rehman <rehmankp186@gmail.com>
2024-09-28 10:26:30 +0500

    complete task for render country details

---
+++ Next Js/my-app/app/country/[country_name]/page.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+
+const page = ({ params }: any) => {
+  const countryInfo: any = {
+    pakistan: {
+      name: "Pakistan",
+      population: "220 million",
+      capital: "Islamabad",
+    },
+    india: {
+      name: "India",
+      population: "1.4 billion",
+      capital: "New Delhi",
+    },
+    usa: {
+      name: "United States of America",
+      population: "331 million",
+      capital: "Washington D.C.",
+    },
+    uk: {
+      name: "United Kingdom",
+      population: "67 million",
+      capital: "London",
+    },
+    afghanistan: {
+      name: "Afghanistan",
+      population: "39 million",
+      capital: "Kabul",
+    },
+  };
+
+  const country =
+    params.country_name.toLowerCase();
+
+  let countryData = countryInfo[country];
+
+  return (
+    <>
+      <ul>
+        <li>Country Name: {countryData.name}</li>
+        <li>
+          Country Population:
+          {countryData.population}
+        </li>
+        <li>
+          Country Capital: {countryData.capital}
+        </li>
+      </ul>
+    </>
+  );
+};
+
+export default page;
+
+// name, population ,capital
